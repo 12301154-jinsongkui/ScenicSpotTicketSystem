9ae66648fc401edd2dc2db54b8b038dae658ebe3
majixian <1608066770@qq.com>
2015-05-07 08:58:03 +0800

    first version

---
+++ SQL脚本/coffee.sql
@@ -0,0 +1,12 @@
+
+
+
+create table t_Coffee(
+
+       id        int PRIMARY KEY auto_increment,
+       name      varchar(15),
+       describe  varchar(30),
+       price1     int,
+       sort      int
+
+)
